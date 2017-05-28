627846d87775cba5b9741398db07a8251a825bdb
Ondrej Toman <tom.ondrej@hotmail.cz>
2017-05-28 12:33:55 +0200

    Updated gitignore and readme

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Ruby timer is and app for tracking time you spend working on projects.
 
 This app is now in early development version. So please don't use it!. It won't work anyway ;-)
+
+###Requiered gems:
+
+* json (gem install json)
+* sqlite3 (gem install sqlite3), you also have to have installed SQLite engine
--- data/data_controller.rb
+++ data/data_controller.rb
@@ -2,6 +2,8 @@ require 'sqlite3'
 
 # Handle operations with database
 class DataController
+
+  attr_accessor :database
   def initialize
     # First of all, test if database file exist or not
     load_database
@@ -11,6 +13,8 @@ class DataController
     file = FilesManager.new('data/data.db')
     if file.exist?
       @database_file = file
+      @database = SQLite3::Database.new 'data/data.db'
+
       puts 'Database file exist!'
     else
       raise 'Critical error. No database file!'
