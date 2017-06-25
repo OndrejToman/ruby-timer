1d76caa312d453d4657e89d213d5a6a51d22da60
Ondrej Toman <tom.ondrej@hotmail.cz>
2017-06-25 15:13:08 +0200

    Some changes

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Ruby timer is and app for tracking time you spend working on projects.
 
 This app is now in early development version. So please don't use it!. It won't work anyway ;-)
 
-###Requiered gems:
+##Requiered gems:
 
 * json (gem install json)
 * sqlite3 (gem install sqlite3), you also have to have installed SQLite engine
--- data/data_controller.rb
+++ data/data_controller.rb
@@ -20,4 +20,36 @@ class DataController
       raise 'Critical error. No database file!'
     end
   end
+
+  def db_insert(table, values)
+    {
+        'one' => 1,
+        'two' => 2,
+    }.each do |pair|
+      @database.execute 'insert into numbers values ( ?, ? )', pair
+    end
+  end
+
+  def db_delete()
+
+  end
+
+  def db_edit()
+
+  end
+
+  # Select database
+  # table -> from which table you want to select
+  # condition -> (WHERE). If condition is empty, method will select all rows
+  def db_select(table, condition)
+    if condition != ''
+      @database.execute("select * from #{table}") do |row|
+        puts row
+      end
+    else
+      @database.execute("select * from times where #{condition}") do |row|
+        puts row
+      end
+    end
+  end
 end
